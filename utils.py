c6bc5a4f36ecef260ad842490dd86f649691bca2
Nazmus-1225 <76961621+Nazmus-1225@users.noreply.github.com>
2024-03-04 13:08:35 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,16 +1,16 @@
-import scipy.misc
+from skimage import transform, io
 import numpy as np
 
 def get_image(image_path, image_size, nb_channels=3, is_crop=True):
-    return transform(imread(image_path, nb_channels), image_size, is_crop)
+    return transformImg(imread(image_path, nb_channels), image_size, is_crop)
 
 def imread(path, nb_channels):
     if nb_channels == 3:
-        return scipy.misc.imread(path, mode='RGB').astype(np.float)
+        return io.imread(path, mode='RGB').astype(np.float)
     elif nb_channels == 1:
-        return scipy.misc.imread(path, mode='L').astype(np.float)
+        return io.imread(path, mode='L').astype(np.float)
 
-def transform(image, npx=96, is_crop=True):
+def transformImg(image, npx=96, is_crop=True):
     # npx : # of pixels width/height of image
     if is_crop:
         cropped_image = center_crop(image, npx)
@@ -27,10 +27,10 @@ def center_crop(x, crop_h, crop_w=None, resize_w=96):
     h, w = x.shape[:2]
     j = int(round((h - crop_h)/2.))
     i = int(round((w - crop_w)/2.))
-    return scipy.misc.imresize(x[j:j+crop_h, i:i+crop_w], [resize_w, resize_w])
+    return transform.resize(x[j:j+crop_h, i:i+crop_w], [resize_w, resize_w])
 
 def imsave(image, path):
-    return scipy.misc.imsave(path, image)
+    return io.imsave(path, image)
 
 def save_images(images, image_path):
     return imsave(inverse_transform(images), image_path)
